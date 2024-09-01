5bda0c7b2f2290b395dae06d0ad29922989d44c4
starduststr <starduststr0@gmail.com>
2024-09-02 01:17:13 +0700

    refactor: ico

--- resources/views/layouts/pasien.blade.php
+++ resources/views/layouts/pasien.blade.php
@@ -6,7 +6,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Klinik Anyadellasis</title>
-    <link rel="shortcut icon" type="image/png" href="{{asset('Logo Digital Pelajar (tanpa teks).png')}}" />
+    <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="https://klinikanyadellasis.com/logo-klinik-anyadellasis.png" />
     <link rel="stylesheet" href="{{asset('assets/css/styles.min.css')}}" />
   <link href="https://cdn.datatables.net/v/dt/dt-2.1.3/datatables.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
