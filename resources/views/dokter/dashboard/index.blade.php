2b0df579b4cff7ba8df6d125fa67ecda33a2bd4b
starduststr <starduststr0@gmail.com>
2024-09-02 00:48:33 +0700

    refactor: dashboard dokter

--- resources/views/dokter/dashboard/index.blade.php
+++ resources/views/dokter/dashboard/index.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.dokter')
+<style>
+    .menu-item {
+        display: flex;
+        align-items: center;
+        font-family: 'Arial', sans-serif; /* Ganti dengan font sesuai keinginan */
+        font-weight: bold;
+        color: #2C3E50; /* Warna teks, ganti sesuai kebutuhan */
+    }
 
+    .divider {
+        width: 2px;
+        height: 100%;
+        background-color: #2C3E50; /* Warna garis, ganti sesuai kebutuhan */
+        margin-right: 8px; /* Jarak antara garis dan teks */
+    }
+
+    .menu-text {
+        font-size: 16px; /* Ukuran teks, sesuaikan dengan kebutuhan */
+    }
+
+</style>
 @section('content')
     <div class="row">
         <div class="col-md-3 col-lg-3 col-12">
@@ -7,9 +27,9 @@
                 <div class="card-body">
                     <div class="d-flex justify-content-between">
                         <i class="fa fa-clipboard-list" style="font-size: 40px"></i>
-                        <h1>17</h1>
+                        <h1>{{ $pasienHariIni->where('status', 'antrian')->count() }}</h1>
                     </div>
-                    <span>Pendaftaran Pasien Hari Ini</span>
+                    <span>Pasien Sedang Antri</span>
                 </div>
             </div>
         </div>
@@ -19,7 +39,7 @@
                 <div class="card-body">
                     <div class="d-flex justify-content-between">
                         <i class="fa fa-calendar-check" style="font-size: 40px"></i>
-                        <h1>12</h1>
+                        <h1>{{ $pasienHariIni->where('status', 'selesai')->count() }}</h1>
                     </div>
                     <span>Pemeriksaan Pasien Hari Ini</span>
                 </div>
@@ -31,9 +51,9 @@
                 <div class="card-body">
                     <div class="d-flex justify-content-between">
                         <i class="fa fa-user-injured" style="font-size: 40px"></i>
-                        <h1>70</h1>
+                        <h1>{{ $totalRekamMedisSelesai->groupBy('id_pasien')->count() }}</h1>
                     </div>
-                    <span>Total Pasien</span>
+                    <span>Total Pasien Anda</span>
                 </div>
             </div>
         </div>
@@ -43,9 +63,9 @@
                 <div class="card-body">
                     <div class="d-flex justify-content-between">
                         <i class="fa fa-user-md" style="font-size: 40px"></i>
-                        <h1>25</h1>
+                        <h1>{{ $totalRekamMedisSelesai->count() }}</h1>
                     </div>
-                    <span>Total Dokter</span>
+                    <span>Total Anda Memeriksa</span>
                 </div>
             </div>
         </div>
@@ -55,37 +75,39 @@
         <div class="col-md-6 col-lg-6 col-12">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title"><i class="fa fa-clipboard"></i> Jadwal Dokter Hari Ini</h5>
+                    <h5 class="card-title mb-3"><i class="fa fa-clipboard"></i> Pasien Perlu Anda Periksa</h5>
         
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title"><i class="fa fa-user-md me-2"></i> Dr. Defa Nugraha</h5>
-                            <p class="card-text mb-2">
-                                <i class="fa fa-calendar-alt me-2"></i><strong>Hari:</strong> Senin, Rabu, Jumat
-                            </p>
-                            <p class="card-text mb-2">
-                                <i class="fa fa-clock me-2"></i><strong>Waktu:</strong> 08:00 - 12:00
-                            </p>
-                            <p class="card-text mb-2">
-                                <i class="fa fa-hospital me-2"></i><strong>Poli:</strong> Poli Umum
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title"><i class="fa fa-user-md me-2"></i> Dr. Seika Nugraha</h5>
-                            <p class="card-text mb-2">
-                                <i class="fa fa-calendar-alt me-2"></i><strong>Hari:</strong> Senin, Rabu, Jumat
-                            </p>
-                            <p class="card-text mb-2">
-                                <i class="fa fa-clock me-2"></i><strong>Waktu:</strong> 08:00 - 12:00
-                            </p>
-                            <p class="card-text mb-2">
-                                <i class="fa fa-hospital me-2"></i><strong>Poli:</strong> Poli Umum
-                            </p>
-                        </div>
-                    </div>
+                    @if(!$pasienHariIni->where('status', 'tindakan'))
+                            <strong class="text-center">Tidak ada</strong>
+                    @else
+                        @foreach($pasienHariIni->where('status', 'tindakan') as $item)
+                            <div class="menu-item mb-2">
+                                <span class="divider"></span>
+                                <span class="menu-text">
+                                    {{ $item->pasien->user->name }}
+                                </span>
+                            </div>
+                            <ul class="list-unstyled mb-2">
+                                <li class="mb-2">
+                                    <i class="fa fa-clock me-2"></i> 
+                                    <strong>Waktu:</strong> {{ $item->created_at->diffForHumans() }}
+                                </li>
+                                <li>
+                                    <i class="fa fa-check-circle me-2"></i> 
+                                    <strong>Status:</strong> 
+                                    <span class="badge bg-{{ $item->status == 'selesai' ? 'success' : 'warning' }} text-uppercase">{{ $item->status }}</span>
+                                </li>
+                                <li>
+                                    <i class="fa fa-comment-dots me-2"></i> 
+                                    <strong>Keluhan:</strong> {{ $item->ammanesia }}
+                                </li>
+                                <li>
+                                    <i class="fa fa-user-md me-2"></i> 
+                                    <strong>Dokter:</strong> {{ $item->dokter->user->name }}
+                                </li>
+                            </ul>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
@@ -93,58 +115,40 @@
         <div class="col-md-6 col-lg-6 col-12">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title"><i class="fa fa-clipboard"></i> Perawatan Hari Ini</h5>
+                    <h5 class="card-title mb-3"><i class="fa fa-clipboard"></i> Pemeriksaan anda Hari Ini</h5>
         
                     <div class="row">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title"><strong>I Rosiana</strong></h5>
-                                <ul class="list-unstyled mb-0">
-                                    <li class="mb-2">
-                                        <i class="fa fa-clock me-2"></i> 
-                                        <strong>Waktu:</strong> 1 jam yang lalu
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-check-circle me-2"></i> 
-                                        <strong>Status:</strong> 
-                                        <span class="badge bg-success">Selesai</span>
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-comment-dots me-2"></i> 
-                                        <strong>Keluhan:</strong> Sasak Nafas
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-user-md me-2"></i> 
-                                        <strong>Dokter:</strong> Defa Nugraha
-                                    </li>
-                                </ul>
+                        @if(!$pasienHariIni->where('status', 'selesai'))
+                            <strong class="text-center">Tidak ada</strong>
+                        @else
+                            @foreach($pasienHariIni->where('status', 'selesai') as $item)
+                            <div class="menu-item mb-2">
+                                <span class="divider"></span>
+                                <span class="menu-text">
+                                    {{ $item->pasien->user->name }}
+                                </span>
                             </div>
-                        </div>
-        
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title"><strong>Seika Nugraha</strong></h5>
-                                <ul class="list-unstyled mb-0">
-                                    <li class="mb-2">
-                                        <i class="fa fa-clock me-2"></i> 
-                                        <strong>Waktu:</strong> 1 jam yang lalu
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-check-circle me-2"></i> 
-                                        <strong>Status:</strong> 
-                                        <span class="badge bg-success">Selesai</span>
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-comment-dots me-2"></i> 
-                                        <strong>Keluhan:</strong> Sasak Nafas
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-user-md me-2"></i> 
-                                        <strong>Dokter:</strong> Defa Nugraha
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                            <ul class="list-unstyled mb-2">
+                                <li class="mb-2">
+                                    <i class="fa fa-clock me-2"></i> 
+                                    <strong>Waktu:</strong> {{ $item->created_at->diffForHumans() }}
+                                </li>
+                                <li>
+                                    <i class="fa fa-check-circle me-2"></i> 
+                                    <strong>Status:</strong> 
+                                    <span class="badge bg-{{ $item->status == 'selesai' ? 'success' : 'warning' }} text-uppercase">{{ $item->status }}</span>
+                                </li>
+                                <li>
+                                    <i class="fa fa-comment-dots me-2"></i> 
+                                    <strong>Keluhan:</strong> {{ $item->ammanesia }}
+                                </li>
+                                <li>
+                                    <i class="fa fa-user-md me-2"></i> 
+                                    <strong>Dokter:</strong> {{ $item->dokter->user->name }}
+                                </li>
+                            </ul>
+                            @endforeach
+                        @endif
                         <hr>
                     </div>
                 </div>
